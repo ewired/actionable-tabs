324cd4d3bec6323df9e09c88297ecea4f2d75b04
ewired <37567272+ewired@users.noreply.github.com>
2025-10-22 00:55:08 -0400

    fix #4 change the defaults

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,7 +1,7 @@
-/**
- * Default settings for the Actionable Tabs extension
- */
-export type Settings = {
+import { nanoid } from "nanoid";
+
+export type Rule = {
+	id: string;
 	cronSchedule: string;
 	queueMode:
 		| "oldest-first"
@@ -14,11 +14,27 @@ export type Settings = {
 	showNotifications: boolean;
 };
 
+/**
+ * Default settings for the Actionable Tabs extension
+ */
+export type Settings = {
+	/**
+	 * Rules for moving tabs. Rules that run on the same schedule will respect
+	 * the order, so there are controls to re-order the rules.
+	 */
+	rules: Rule[];
+};
+
 export const DEFAULTS: Settings = {
-	cronSchedule: "*/30 * * * *", // every 30 minutes
-	queueMode: "leftmost-first",
-	lastMoveTime: null,
-	moveCount: 1, // how many actionable tabs to move per cron execution
-	moveDirection: "left", // where to move actionable tabs: 'left' (after pinned tabs) or 'right' (end of tab strip)
-	showNotifications: true,
+	rules: [
+		{
+			id: nanoid(),
+			cronSchedule: "*/30 * * * *", // every 30 minutes
+			queueMode: "leftmost-first",
+			lastMoveTime: null,
+			moveCount: 1, // how many actionable tabs to move per cron execution
+			moveDirection: "left", // where to move actionable tabs: 'left' (after pinned tabs) or 'right' (end of tab strip)
+			showNotifications: true,
+		},
+	],
 };
